10519997c23b2d2b44b4cd0ad9ec24e41a857aeb
methodoti <marcelo@patricio.net.br>
2026-03-19 10:57:05 -0300

    Refactor setSectionSelection function to be a named export

--- week3/scripts/sections.mjs
+++ week3/scripts/sections.mjs
@@ -1,9 +1,20 @@
-function setSectionSelection() {
-  const sectionSelect = document.querySelector("#sectionNumber");
-  byuiCourse.sections.forEach((section) => {
-    const option = document.createElement("option");
-    option.value = section.sectionNumber;
-    option.textContent = `${section.sectionNumber}`;
-    sectionSelect.appendChild(option);
-  });
-}
+//// function setSectionSelection() { // ===>>> don't need this anyomre, now it's a named export!
+    // ===>>> moved this to export as a nemd export
+//   const sectionSelect = document.querySelector("#sectionNumber");
+//   byuiCourse.sections.forEach((section) => {
+//     const option = document.createElement("option");
+//     option.value = section.sectionNumber;
+//     option.textContent = `${section.sectionNumber}`;
+//     sectionSelect.appendChild(option);
+//   });
+//// }
+
+export function setSectionSelection(sections) { 
+    const sectionSelect = document.querySelector("#sectionNumber");
+    byuiCourse.sections.forEach((section) => {
+        const option = document.createElement("option");
+        option.value = section.sectionNumber;
+        option.textContent = `${section.sectionNumber}`;
+        sectionSelect.appendChild(option);
+    });
+ }
