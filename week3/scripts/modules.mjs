812c42377549cfcd22b3f576f6627d92353c62ce
methodoti <marcelo@patricio.net.br>
2026-03-19 11:35:07 -0300

    Refactor modules.mjs: streamline event listener functions and ensure renderSections is called after enrollment changes

--- week3/scripts/modules.mjs
+++ week3/scripts/modules.mjs
@@ -1,18 +1,28 @@
+// its a default import, without { }
+import byuiCourse from './course.mjs';
+import { setSectionSelection } from './sections.mjs';
+import { setTitle, renderSections } from './output.mjs';
 
 
 
-
-
-
+// reduced to event listeners
 document.querySelector("#enrollStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
-  byuiCourse.changeEnrollment(sectionNum);
-});
-document.querySelector("#dropStudent").addEventListener("click", function () {
-  const sectionNum = Number(document.querySelector("#sectionNumber").value);
-  byuiCourse.changeEnrollment(sectionNum, false);
+    byuiCourse.changeEnrollment(sectionNum);
+    // Add renderSections(this.sections); to both event listeners in order 
+    // to update the output after the enroll or drop button is clicked.
+    renderSections(byuiCourse.sections);
 });
 
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    byuiCourse.changeEnrollment(sectionNum, false);
+    // Add renderSections(this.sections); to both event listeners in order 
+    // to update the output after the enroll or drop button is clicked.
+    renderSections(byuiCourse.sections);
+});
+
+// function calls
 setTitle(byuiCourse);
 setSectionSelection(byuiCourse.sections);
 renderSections(byuiCourse.sections);
