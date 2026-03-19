7af45a724ae79891a7a107f459b9dbc4c4f3c433
methodoti <marcelo@patricio.net.br>
2026-03-19 11:00:01 -0300

    Update setSectionSelection function, remove the byuiCourse. from the beginning of that line

--- week3/scripts/sections.mjs
+++ week3/scripts/sections.mjs
@@ -11,7 +11,7 @@
 
 export function setSectionSelection(sections) { 
     const sectionSelect = document.querySelector("#sectionNumber");
-    byuiCourse.sections.forEach((section) => {
+    sections.forEach((section) => {
         const option = document.createElement("option");
         option.value = section.sectionNumber;
         option.textContent = `${section.sectionNumber}`;
