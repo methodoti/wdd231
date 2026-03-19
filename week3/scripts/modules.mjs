05ff4aeb6f24f186834e1c0f7bb3cf4a65cfc2f4
methodoti <marcelo@patricio.net.br>
2026-03-19 11:18:36 -0300

    Refactor setTitle and renderSections functions to be named exports and update their implementation

--- week3/scripts/modules.mjs
+++ week3/scripts/modules.mjs
@@ -1,19 +1,8 @@
 
 
-function setTitle(course) {
-  document.querySelector("#courseName").textContent = course.name;
-  document.querySelector("#courseCode").textContent = course.code;
-}
 
-function renderSections(sections) {
-  const html = sections.map(
-    (section) => `<tr>
-    <td>${section.sectionNumber}</td>
-    <td>${section.enrolled}</td>
-    <td>${section.instructor}</td></tr>`
-  );
-  document.querySelector("#sections").innerHTML = html.join("");
-}
+
+
 
 document.querySelector("#enrollStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
---
+++ week3/scripts/output.mjs
@@ -0,0 +1,31 @@
+//// function setTitle(course) { // ===>>> don't need this anyomre, now it's a named export!
+    // // ===>>> moved this to export as a nemd export
+  // document.querySelector("#courseName").textContent = course.name;
+  // document.querySelector("#courseCode").textContent = course.code;
+//// }
+
+function renderSections(sections) {
+  const html = sections.map(
+    (section) => `<tr>
+    <td>${section.sectionNumber}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.instructor}</td></tr>`
+  );
+  document.querySelector("#sections").innerHTML = html.join("");
+}
+
+export function setTitle(course) {
+
+    document.querySelector("#courseName").textContent = course.name;
+    document.querySelector("#courseCode").textContent = course.code;
+}
+ 
+export function renderSections(sections) {
+    const html = sections.map( 
+        (section) => `<tr>
+        <td>${section.sectionNumber}</td>
+        <td>${section.enrolled}</td>
+        <td>${section.instructor}</td></tr>`
+    );
+    document.querySelector("#sections").innerHTML = html.join("");
+}
