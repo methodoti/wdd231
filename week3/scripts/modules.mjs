3ec24b2f73bf0dc7cdf8250f0ce845c26e77a623
methodoti <marcelo@patricio.net.br>
2026-03-19 10:47:48 -0300

    move the setSectionSelection function form modules file into the sections file

--- week3/scripts/modules.mjs
+++ week3/scripts/modules.mjs
@@ -1,12 +1,4 @@
-function setSectionSelection() {
-  const sectionSelect = document.querySelector("#sectionNumber");
-  byuiCourse.sections.forEach((section) => {
-    const option = document.createElement("option");
-    option.value = section.sectionNumber;
-    option.textContent = `${section.sectionNumber}`;
-    sectionSelect.appendChild(option);
-  });
-}
+
 
 function setTitle(course) {
   document.querySelector("#courseName").textContent = course.name;
