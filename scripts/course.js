e5c2b422a34814360fa3ff923d5ffba85d563bfc
methodoti <marcelo@patricio.net.br>
2026-03-28 14:24:31 -0300

    Add coment to couse.js for better understanding; add a event listener test to the buttons

--- scripts/course.js
+++ scripts/course.js
@@ -78,14 +78,24 @@ const courses = [
     }
 ]
 
-const button = document.querySelectorAll(".button-courses");
-const numCredits = document.querySelector("#num-credits");
+const button = document.querySelectorAll(".button-courses"); // All | WDD | CSE
+const numCredits = document.querySelector("#num-credits");   // the total credits for course listed
+// for the DIALOG
+const courseDetails = document.querySelector("#course-details");
+const courseDetailsTitle = document.querySelector("#course-details h1");
+const closeButton = document.querySelector("#closeModal");
+
+// CLOSE EVENT LISTNER: it will be the only one for all modals
+closeButton.addEventListener("click", () => {
+    courseDetails.close();
+});
 
 
-let courseToFilter = "All"
-let filteredCourses = courses.filter(filterCourse);
+let courseToFilter = "All"  // DEFAULT BUTTON SELECTION: in this first stage, the filterCourses will return All.
+let filteredCourses = courses.filter(filterCourse); //  create filteredCourses array based on the couseToFilter variable, usig the callback function filterCourses()
 button[0].classList.add("active");
 
+// Call the cardCourse() function that will create all COURSES BUTTONS.
 cardCourse();
 
 for (let index = 0; index < button.length; index++) {
@@ -122,36 +132,52 @@ for (let index = 0; index < button.length; index++) {
     });
 }
 
+// Function responsible for the COURSES BUTTONS CREATION
 function cardCourse() {
-    const arraySubject = document.querySelector("#array-subject");
-    arraySubject.innerHTML = "";
+    const arraySubject = document.querySelector("#array-subject"); // grab the reference to the DIV on the COUSES BUTTONS AREA
+    arraySubject.innerHTML = ""; // clean the DIV for not showing button repetitions.
     
-    const totalCredits = filteredCourses.reduce(function (acumulator, course) {
+    const totalCredits = filteredCourses.reduce(function (acumulator, course) { // sum the credits from the current element on the filteredCourses array
         return acumulator + course.credits;
     }, 0);
 
-    for (let index = 0; index < filteredCourses.length; index++) {
-        const element = filteredCourses[index].subject;
+    console.log(filteredCourses); // just to see the filteredCourses array is ok
 
-        // const subjects = document.createElement("span");
-        const subjects = document.createElement("button");
-        // subjects.setAttribute("type", "button");
-        subjects.classList.add("subjectCards");
-        if (filteredCourses[index].completed) {
-                subjects.classList.add("subjectCompleted");
-            subjects.textContent = `✓ ${filteredCourses[index].subject} ${filteredCourses[index].number}`;
-            } else {
-            subjects.textContent = `${filteredCourses[index].subject} ${filteredCourses[index].number}`;
-            }
-        arraySubject.append(subjects);
+    for (let index = 0; index < filteredCourses.length; index++) {      // iterate on the filteredCourses elements
+
+        console.log(filteredCourses[index]);  // just to see the filteredCourses current element
+
+        //const element = filteredCourses[index].subject;  // takes the subject: CSE | WDD
+
+        // const subjects = document.createElement("span");  // old way: span
+        const subjectButton = document.createElement("button");  // new way: button, for the click event magic!
+        subjectButton.classList.add("subjectCards");             // add the subjectCards class to the subjectButton for styling
+
+        if (filteredCourses[index].completed) {         // check if the current course element is completed or not to add the '✓' simbole 
+            subjectButton.classList.add("subjectCompleted");
+            subjectButton.textContent = `✓ ${filteredCourses[index].subject} ${filteredCourses[index].number}`;
+        } else {
+            subjectButton.textContent = `${filteredCourses[index].subject} ${filteredCourses[index].number}`;
+        }
+
+        subjectButton.addEventListener("click", () => showStuff(filteredCourses[index]));
+        
+        arraySubject.append(subjectButton);  // add the subjectButton to the DIV on the COUSES BUTTONS AREA
     }
-    numCredits.textContent = `The total credits for course listed above is ${totalCredits}`;
+    numCredits.textContent = `The total credits for course listed above is ${totalCredits}`;  // display the total credits
 }
 
+// Function to filter the courses
 function filterCourse(course) {
-    if (courseToFilter === "All") {
+    if (courseToFilter === "All") { // if All, it returns the original courses array.
         return courses;
-    } else {
+    } else {                       // else, it returns all courses with the subject = courseToFilter variable.
         return course.subject === courseToFilter;
     }
+}
+
+// for the DIALOG
+function showStuff(element) {
+    courseDetailsTitle.innerHTML = `${element.subject} ${element.number}`;
+    courseDetails.showModal();
 }
