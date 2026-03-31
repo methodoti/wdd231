17a75a890a8c3a9ae93e552064693bdcce32cbbe
methodoti <marcelo@patricio.net.br>
2026-03-31 19:22:03 -0300

    Create form on join page; create thank you page; implement date code for the hiddenfield

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -6,3 +6,10 @@ currentYearSpan.textContent = new Date().getFullYear();
 
 const lastModified = document.lastModified;
 lastModifiedSpan.textContent = lastModified;
+
+// to get the time of the page load on join.html timestamp hidden input
+const timestamp = document.querySelector("#timestamp");
+if (timestamp) {
+    document.getElementById('timestamp').value = new Date().toISOString();
+}
+
